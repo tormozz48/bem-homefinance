c48405819748498273f800ff6d678b698ac59ae8
Bemer <bemer@yandex-team.ru>
2014-05-22 00:43:01 +0400

    Add enb server-middleware to express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
-var express = require('express');
+var express = require('express'),
+    enbMiddleware = require('enb/lib/server/server-middleware');
+
+
 var app = express();
+
+app.use(enbMiddleware.createMiddleware({
+    cdir: process.cwd(),
+    noLog: false
+}));
 
 app.get('/', function(req, res){
     res.send('Hello World');
